789a480a2c2b7e87d89e97d123851e70fb2f538c
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-20 22:05:17 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,11 +5,11 @@ i = 0
 while i < collection.length #while i < 100
 yield(collection[i]) #yield(1)
 return collection[i]
-end 
+end
   i = i + 1
 end
 end
-
+#if block returns true, the argument passed into yield, will exit loop and will be the return value of the method. if false, continue to next element. 
 collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0}
 #when yield(1) => false
