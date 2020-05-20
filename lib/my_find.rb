d1f70d6ded60acf1bb0573207a37b2ae4a9f7307
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-20 22:03:36 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def my_find(collection)
 i = 0
-while i < collection.length
-yield(collection[i])
+while i < collection.length #while i < 100
+yield(collection[i]) #yield(1)
   i = i + 1
 end
 end
