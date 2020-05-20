be069858650dbed58b3a10e8d3dedae5249e4ea1
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-20 22:04:11 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,10 +4,11 @@ def my_find(collection)
 i = 0
 while i < collection.length #while i < 100
 yield(collection[i]) #yield(1)
+return collection[i]
   i = i + 1
 end
 end
 
 collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 and i % 5 == 0}
-#when yield(1) => false 
+#when yield(1) => false
